3c728f814062be0fcc4c699dc58e932ed5b0b04c
Laíza  Pimentel <laizahpimentel@gmail.com>
2024-12-23 00:56:30 -0300

    23/12

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import { FiSearch } from 'react-icons/fi'
 
 function App() {
   return (
-    <div className="App">
-  
+    <div className="container">
+      <h1 className="tile"> Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep..." />
+      
+      <button className="buttonSearch">
+        <FiSearch  size={25} color='#FFF'/>
+      </button>
+      </div>
+      
+      <main className='main'>
+        <h2>CEP: 79003222</h2>
+
+      </main>
+    
     </div>
   );
 }
