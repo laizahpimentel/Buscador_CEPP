6fc74bc3feffc768d9c1046f974f9a6d4e5748af
Laíza  Pimentel <laizahpimentel@gmail.com>
2024-12-30 14:29:27 -0300

    finished application web buscador web, add API cep

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi'
 import './styles.css';
 
+import api from './services/api'
+
 function App() {
 
-  const[input, setInput] = useState('')
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
 
 
-  function handleSearch(){
-    // alert("Clicou!")
-    alert("VALOR DO INPUT" + input)
+  async function handleSearch() {
+    //01310930/json/
+
+    if (input === '') {
+      alert("Preencha algum cep.")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      // console.log(response.data)
+      setCep(response.data)
+      setInput("");
+    } catch {
+      alert("Ops! erro ao buscar");
+      setInput("");
+
+    }
   }
 
   return (
@@ -18,27 +36,31 @@ function App() {
 
       <div className="containerInput">
         <input
-        type="text"
-        placeholder="Digite seu cep..."
-        value={input}
-        onChange={(e) => setInput(e.target.value) }
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
-      
-      <button className="buttonSearch" onClick={handleSearch}>
-        <FiSearch  size={25} color='#FFF'/>
-      </button>
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='#FFF' />
+        </button>
       </div>
-      
-      <main className='main'>
-        <h2>CEP: 79003222</h2>
 
-        <span> Rua teste algum</span>
-        <span>Complemento: algum complemento</span>
-        <span>Jardim Conceicao</span>
-        <span>Campinas-sp </span>
 
-      </main>
-    
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep} </h2>
+
+          <span> {cep.logradouro} </span>
+          <span>complemento: {cep.complemento} </span>
+          <span> {cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf} </span>
+
+        </main>
+      )}
+
+
     </div>
   );
 }
